e881974650155d2f57297ecf8e7bb6ee0f60052c
GitHubUtile <159071279+GitHubUtile@users.noreply.github.com>
2024-02-06 19:23:34 +0200

    Add files via upload

---
+++ JSportfolio.js
@@ -0,0 +1,26 @@
+//mouse
+const circle = document.getElementById('circle');
+circle.style.pointerEvents = 'none';
+
+document.addEventListener('mousemove', (e) => {
+    const height = circle.offsetHeight;
+    const width = circle.offsetWidth;
+
+    setTimeout(function() { 
+        circle.style.left = `${e.clientX - width/2}px`;
+        circle.style.top = `${e.clientY - height/2}px`;
+    }, 35);
+});
+
+//Alternating text
+const texts = ['Welcome!', 'Bienvenue!', 'Bienvenido!', '!أهلا بكم'];
+const element = document.getElementById('alternating-text');
+
+let i = parseInt(localStorage.getItem('currentIndex')) || 0;
+const listener = e => {
+  i = i < texts.length - 1 ? i + 1 : 0 ;
+  localStorage.setItem('currentIndex', i)
+  element.innerHTML = texts[i];
+};
+element.innerHTML = texts[i];
+element.addEventListener( 'animationiteration' , listener, false);
